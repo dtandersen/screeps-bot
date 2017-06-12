e44d4f368ed53ed5854c9dc2f068a2d9f9648d7a
David Andersen <git@davidandersen.us>
2017-06-12 15:20:26 -0700

    lodash test

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,10 @@
 import { Expect, Test } from "alsatian";
+import * as _ from "lodash";
 
 export class ExampleTestFixture {
-
   @Test("Confirm 1 + 1 is 2")
   public test1() {
     Expect(1 + 1).toBe(2);
+	_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 });
   }
 }
